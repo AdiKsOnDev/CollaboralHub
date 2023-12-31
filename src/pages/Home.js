2d82ca9c65ddb2704546a7f1bf5884b61687308b
aa2152 <aa2152@hw.ac.uk>
2023-12-31 21:35:45 +0400

    feat: Homepage features
    Made starter codes for the homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeBox from '../components/HomeBox';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to ThinkLabs</h1>
+      <HomeBox />
+    </div>
+  );
+}
+
+export default Home;
