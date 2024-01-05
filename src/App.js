77314e124b09eb024c7a787040edb223e1dac326
aa2152 <aa2152@hw.ac.uk>
2024-01-05 23:30:05 +0400

    feat: Canvas very basic functions
    You can now draw lines and rectangles in the canvas!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // Pages
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Canvas from './pages/Canvas';
 
 function App() {
   return (
@@ -12,10 +13,13 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
+          <Route path="/Canvas" element={<Canvas />} />
         </Routes>
       </div>
     </Router>
+
   );
 }
+
 
 export default App;
---
+++ src/pages/Canvas.js
@@ -0,0 +1,11 @@
+import CanvasBox from '../components/CanvasBox';
+
+function Canvas() {
+    return ( 
+      <div className="w-full h-full flex items-center justify-center">
+        <CanvasBox />
+      </div>
+    );
+}
+
+export default Canvas;
