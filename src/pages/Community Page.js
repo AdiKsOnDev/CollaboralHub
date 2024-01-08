0a885e844b4ca0e98ae56b7af62efb1024c62cd9
Areej Ahmed <aa2149@hw.ac.uk>
2024-01-09 01:42:23 +0400

    refactor: Added components into separate files

--- src/pages/Community Page.js
+++ src/pages/Community Page.js
@@ -1,41 +1,15 @@
 import React from 'react';
-import {firestore} from '../firebase';
-import { addDoc, collection } from '@firebase/firestore';
 import PostHolder from "../components/PostHolder";
-
+import NewPost from "../components/NewPost";
 
 export default function Community() {
 
-    const postRef= React.useRef();
-    const ref = collection( firestore, "posts");
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        console.log(postRef.current.value);
-        
-        let data = {
-            message: postRef.current.value,
-        }
-
-        try {
-            addDoc(ref, data);
-        }
-        catch(e) {
-            console.log("Error adding document: ", e);
-        }
-    };
     return (
-        <div class="newPost-container">
-            
-            <form onSubmit={handleSubmit}>
-         
-                <textarea type="text" class="newPostInput" ref={postRef} placeholder="Scribble away..." />
-                 {/* add images later */}
-                 <img src="" alt="profile img " style={{resizeMode: 'center', width: 61 , height: 61 ,   borderColor: 'red', borderWidth: 4,borderTopRightRadius:270, borderBottomRightRadius: 270, borderTopLeftRadius:270, borderBottomLeftRadius:270 , position: 'absolute',top: 20,  left: 18, }}/>          
-                <button type="submit" class="newPostButton" >Save</button>
-            </form>
-
+        <div>
+            <NewPost />
             <PostHolder />
         </div>
+       
 
     );
 }
