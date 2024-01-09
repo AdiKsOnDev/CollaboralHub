bc02bbd5aeebc18e110641062ae3a27ee06a5838
aa2152 <aa2152@hw.ac.uk>
2024-01-09 23:41:26 +0400

    feat: We can move the elements

--- src/pages/Canvas.js
+++ src/pages/Canvas.js
@@ -1,11 +1,11 @@
 import CanvasBox from '../components/CanvasBox';
 
 function Canvas() {
-    return ( 
-      <div className="w-full h-full flex items-center justify-center">
-        <CanvasBox />
-      </div>
-    );
+  return (
+    <div style={{ backgroundColor: 'white' }} className="w-full h-full flex items-center justify-center">
+      <CanvasBox />
+    </div>
+  );
 }
 
 export default Canvas;
