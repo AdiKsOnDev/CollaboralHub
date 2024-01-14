a27f20a0de18e61bfbd8efb6c33cad489fd170f6
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-14 12:44:07 +0400

    feat: Base code for the navbar
    TODO: Style the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
+import LogoPNG from "../Assets/logo.png"
+
 function Navbar() {
   return (
     <nav className="flex flex-col justify-center items-center background-secondary">
-
+       <ul>
+        <li><LogoPNG /></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Projects</a></li>
+      </ul>
     </nav> 
   );
 }
