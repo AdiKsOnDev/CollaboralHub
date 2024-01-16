d48fbcfefb0bc0d92ab0f386eb21b2838eab3dec
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-17 00:35:31 +0400

    feat: Add a hover to Canvas
    Canvas components become bigger while hovering
    and text size is also modified on hover

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,6 +1,6 @@
 function Canvas({image, title}) {
   return (
-    <div className="flex flex-col items-center justify-center w-48">
+    <div className="flex flex-col items-center justify-center w-48 hover:w-52 hover:text-lg duration-300">
       <img className="w-full rounded-lg" src={image} alt="Canvas" />
 
       <h1 className="text-text-color font-semibold w-full pl-2 mt-3">{title}</h1>
--- src/components/HomeBox.js
+++ src/components/HomeBox.js
@@ -14,6 +14,6 @@ function HomeBox() {
       </div>
     </div>
   );
-}
+};
 
 export default HomeBox;
