52a10e11403aea96a8750b797f9ebe9aa855a5c8
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-17 15:04:30 +0400

    refactor: Changed accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         'secondary':'#2A2A2A',
         'placeholder':'#7E7E7E',
         'text-color':'#FFFFFF',
-        'accent-red':'#EC2C40',
+        'accent-red':'#AE3C48',
         'accent-blue':'#00A9E5',
         'dark-red': '#940513'
       }
