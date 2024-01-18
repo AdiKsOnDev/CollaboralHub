61da4bb07d84abd4a3c6891f865974fbb5010c96
Rohan <rohanadvani19@gmail.com>
2024-01-18 20:22:31 +0400

    feat: new feature

---
+++ src/components/TODO_List.js
@@ -0,0 +1,7 @@
+const TODOList = () => {
+    return (
+        <div class="text-white text-4xl font-poppins font-semibold tracking-wider break-words">TODO Lists </div>
+    );
+  };
+  
+  export default TODOList;
---
+++ src/pages/sticky_notes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TODOList from "../components/TODO_List";
+
+export default function sticky_notes() {
+
+    return (
+        <div>
+            <TODOList />
+        </div>
+    );
+}
