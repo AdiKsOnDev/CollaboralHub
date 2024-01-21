489cc814b3525139973c998e056cb04f51e56cd7
Areej Ahmed <aa2149@hw.ac.uk>
2024-01-21 21:54:12 +0400

    feat: News-feed completed with data-fetch from firestore

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,40 +1,52 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import { database } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
+async function getNews() {
+  const querySnapshot = await getDocs(collection(database, 'NewsFeed'));
+  const news=[];
+
+  querySnapshot.forEach((doc) => {
+    news.push({ id:doc.id, ...doc.data() });
+    console.log(doc.id, ' => ', doc.data());
+  })
+  return news;
+}
 const NewsFeed = () => {
   const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const collectionRef = database.collection('NewsFeed');
-        const snapshot = await collectionRef.get();
-
-        const data = snapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-
-        setNewsData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
+    async function fetchData() {
+      const news = await getNews();
+      setNewsData(news);
+    }
     fetchData();
   }, []);
-
   return (
-    <div className="news-container h-full ">
-      <div className="main-title"> Community News </div>
-      {newsData.map(news => (
-        <div key={news.id} className="news-item">
-          <div className="news-title">{news.title}</div>
-          <div className="news-body">{news.body}</div>
-        </div>
-      ))}
+    <div className="news-container flex min-h-screen flex-col">
+      <div className="main-title "> Community News </div>
+      <div className=" left-[25px] top-[70px] absolute">  
+        {newsData.map((news) => (
+          <div key={newsData.id} className='mb-4 w-[209px] pb-[40px] relative'> 
+
+            <p className="text-white text-[10px] font-bold font-['Poppins'] underline tracking-wide"> {news.newsTitle}</p>
+            <p className="text-white absolute text-[10px] font-normal font-['Poppins'] tracking-wide">{news.newsBody}</p>
+            <p> </p>
+          </div>
+          ))}
+      </div>
     </div>
   );
 };
 
 export default NewsFeed;
+
+{/* <div >  
+{newsData.map((news) => (
+  <div key={news.id} className='mb-4'> 
+    <p className="news-title">{news.title}</p>
+    <p className="news-body">{news.id.body}</p>
+  </div>
+  ))}
+</div> */}
