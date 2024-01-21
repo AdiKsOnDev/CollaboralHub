8a86cf6007c0bc6e80fe5267007baa76bfa47f69
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-21 15:38:55 +0400

    fix: Fixed protected access
    User can use the website only when authorised

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import { getAuth } from "firebase/auth";
 import { fireApp } from './firebase.js';
+
+import { AuthContext } from "./context/AuthContext";
 
 // Pages
 import Login from './pages/Login';
@@ -8,31 +12,28 @@ import Register from './pages/Register';
 import Navbar from './components/Navbar.js';
 
 function App() {
-  const auth = getAuth(fireApp);
+  const currentUser = useContext(AuthContext);
 
-  if (auth.currentUser) {
-    return (
-      <Router>
-        <div className="App bg-primary w-screen h-screen">
-          <Routes>
-            <Route path="/" element={<Navbar />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  } else {
-    return (
-      <Router>
-        <div className="App bg-primary w-screen h-screen">
-          <Routes>
-            <Route path="*" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
-    )
+  console.log(currentUser);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/" />
+    }
+
+    return children;
   }
-}
-
+  return (
+    <Router>
+      <div className="App bg-primary w-screen h-screen">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/navbar" element={<ProtectedRoute><Navbar /></ProtectedRoute>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 export default App;
 
