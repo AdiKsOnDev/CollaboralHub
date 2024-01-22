b9c79fbf1a983ec389ef64423ac210aa8ed7030c
Areej Ahmed <aa2149@hw.ac.uk>
2024-01-22 09:31:30 +0400

    feat:Data fetch set up for posts

--- src/components/PostHolder.js
+++ src/components/PostHolder.js
@@ -1,15 +1,57 @@
-const PostHolder = () => {
+'use client';
+import React, { useEffect, useState } from 'react';
+import { database } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+async function getPosts() {
+  const querySnapshot = await getDocs(collection(database, "Posts"));
+  const post=[];
+
+  querySnapshot.forEach((doc) => {
+    post.push({ id:doc.id, ...doc.data() });
+    console.log(doc.id, ' => ', doc.data());
+  })
+  return post;
+}
+const PostHolder= () => {
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const post = await getPosts();
+      setPostData(post);
+    }
+    fetchData();
+  }, []);
+
     return (
-        <div className="post-container">
+        <div >
+            
+
+        {/* <div className="post-container"> */}
       
             {/* add images later */}
-            <img src="" alt="profile img " style={{resizeMode: 'center', width: 61 , height: 61 ,   borderColor: 'red', borderWidth: 4,borderTopRightRadius:270, borderBottomRightRadius: 270, borderTopLeftRadius:270, borderBottomLeftRadius:270 , position: 'absolute',top: 20,  left: 18, }}/>
+            {/* <img src="" alt="profile img " style={{resizeMode: 'center', width: 61 , height: 61 ,   borderColor: 'red', borderWidth: 4,borderTopRightRadius:270, borderBottomRightRadius: 270, borderTopLeftRadius:270, borderBottomLeftRadius:270 , position: 'absolute',top: 20,  left: 18, }}/>
     
             <div className="post-username"> Demo </div>
             <div className="post-content"> lorem ipsum  </div>
             <div className="post-contentimg">  </div>
+        </div> */}
+
+
+       
+            {postData.map((post) => (
+                <div key={postData.id} > 
+                    <div className="post-container pb-100">  
+                        <p className="post-username"> {post.userName}</p>
+                        <p className="post-content">{post.postBody}</p>
+                        <p className="post-contentimg"> </p>
+                    </div>
+                </div>
+            ))}
         </div>
+
     );
   };
-  
+
   export default PostHolder;
