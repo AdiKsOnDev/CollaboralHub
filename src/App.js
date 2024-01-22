9898c9e11b984305bd7ae63625263c660aa0c5c6
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-23 02:01:22 +0400

    fix: Added the actual search box
    @AdiKsOnDev Branch is ready to be merged

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Navbar from './components/Navbar';
-import StatusBar from './components/StatusBar';
-import Community from './pages/Community';
 
 function App() {
   return (
     <Router>
       <div className="App bg-primary w-screen h-screen">
         <Routes>
-          <Route path="/" element={<Community />} />
           <Route path="/" element={<Navbar />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
