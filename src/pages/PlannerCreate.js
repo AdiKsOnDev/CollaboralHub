72a8bec81b69949a8608ed19f90b050440b19bc5
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-23 01:55:50 +0400

    feat: Added a SearchBar

--- src/pages/PlannerCreate.js
+++ src/pages/PlannerCreate.js
@@ -3,17 +3,25 @@ import { getMonth } from '../components/Planner/PlannerUtil.js'
 import PlannerSidebar from '../components/Planner/PlannerSidebar.js';
 import PlannerMonth from '../components/Planner/PlannerMonth.js';
 import Navbar from '../components/Navbar.js';
+import StatusBar from '../components/StatusBar.js';
 
 function PlannerCreate() {
   const [currentMonth, setCurrentMonth] = useState(getMonth())
 
   return (
     <React.Fragment>
-      <div className="h-screen flex flex-columns">
+      <div className="h-screen flex">
         <Navbar />
-        <div className="flex flex-1 bg-secondary">
-          <PlannerSidebar />
-          <PlannerMonth month={currentMonth} />
+
+        <div className="flex flex-columns bg-primary w-full flex-col">
+          <StatusBar />
+
+          <div className="p-12 pb-0 h-full">
+            <div className="flex flex-1 h-full bg-secondary">
+              <PlannerSidebar />
+              <PlannerMonth month={currentMonth} />
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
