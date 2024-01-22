455e3d5af68c25f1dddf2b28b24cf2a7c252e0ec
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-22 22:30:03 +0400

    feat: Made the canvas dark themed

--- src/pages/Canvas.js
+++ src/pages/Canvas.js
@@ -1,12 +1,13 @@
 import CanvasBox from '../components/CanvasBox';
 import CanvasHeader from '../components/CanvasHeader.js';
 import { Excalidraw } from "@excalidraw/excalidraw";
+import { THEME } from "@excalidraw/excalidraw";
 
 function Canvas() {
   return (
     <div className="w-full h-full flex flex-col">
       <CanvasHeader />
-      <Excalidraw />
+      <Excalidraw theme="dark" />
     </div>
   );
 }
