8c91f0fde78fb526bbb6aceab04162f0ac873e87
AdiKsOnDev <adilalizade13@gmail.com>
2024-01-26 00:49:42 +0400

    feat: Made different API routes to interact with firestore

---
+++ server/server.js
@@ -0,0 +1,87 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { collection, query, where, getDocs, doc, setDoc } from "firebase/firestore";
+import { database } from "./firebase.js";
+
+// dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+/** GET 
+ *
+ * @return index.html
+ */
+app.get("/", async(req, res) => {
+    res.sendFile('index.html', { root: "../build" });
+});
+
+/** GET `canvases`
+ */
+// app.get("/api/canvases", async(req, res) => {
+//     try {
+//         const projects = await Project.find({}).sort({ _id: -1 });
+//         res.status(200).json(projects);
+//     } catch (error) {
+//         res.status(404).json({ msg: "Data could not be found." });
+//     }
+// });
+
+/** GET `currentUser`
+  *
+  * @param {[string]} email
+  *
+  * @return {[JSON]} 
+  */
+app.get("/api/currentUser", async(req, res) => {
+    try {
+        const email = req.body;
+
+        if (email) {
+          const user = query(collection(database, "Users"), where("email", "==", email));
+    }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ msg: "Data could not be found." });
+    }
+});
+
+/** POST `register` 
+  *
+  * @param {[string]} email  
+  * @param {[string]} name  
+  * @param {[string]} lastname 
+  *
+  * @return HTTP status code
+  */
+app.post("/api/register", async(req, res) => {
+    try {
+        const {email, name, lastname} = req.body;
+        const newUser = {
+            email,
+            name,
+            lastname,
+        };
+
+        if (newUser) {
+            const user = await setDoc(doc(database, "Users", name), newUser); 
+
+            res.status(201).json(user);
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: "Sorry, something is wrong with your data.",
+            err: error.message
+        });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening to the server at http://localhost:${port}`);
+}); 
