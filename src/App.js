1fd33e37c92323f39dda9e4839456ade71cd1ea4
Latheef <99171842+Lathoofy18@users.noreply.github.com>
2024-01-27 00:24:23 +0400

    fix: Only authorized users can access

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { getAuth } from "firebase/auth";
 import { fireApp } from './firebase.js';
 
@@ -7,22 +7,35 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Canvas from './pages/Canvas';
 import Navbar from './components/Navbar.js';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <Router>
       <div className="App bg-primary w-screen h-screen">
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          {/* Public Routes */}
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
-          <Route path="/Canvas" element={<Canvas />} />
+
+          {/* Protected Routes */}
+          {currentUser ? (
+            <>
+              <Route path="/" element={<Navbar />} />
+              <Route path="/Canvas" element={<Canvas />} />
+            </>
+          ) : (
+            <Navigate to="/Login" replace />
+          )}
         </Routes>
       </div>
     </Router>
   );
 }
-
 
 export default App;
 
