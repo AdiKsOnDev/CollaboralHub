38ecd820d2e19dfc0214e4834dbb69f302cad9cd
Rohan <rohanadvani19@gmail.com>
2024-01-30 18:35:01 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import PlannerContextWrapper from './PlannerContext/PlannerContextWrapper';
+import reportWebVitals from './PlannerContext/PlannerReportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AuthContextProvider>
+      <PlannerContextWrapper>
+        <App />
+      </PlannerContextWrapper>
+    </AuthContextProvider>
+
+  </React.StrictMode >
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
