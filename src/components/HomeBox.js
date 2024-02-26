eb3d10550d41e57add418adc4b8d6d0551fe6172
AdiKsOnDev <adilalizade13@gmail.com>
2024-02-26 10:49:37 +0400

    fix: Added a test-id

--- src/components/HomeBox.js
+++ src/components/HomeBox.js
@@ -7,7 +7,7 @@ function HomeBox() {
     <div className="flex flex-col w-3/4 h-full bg-primary overflow-scroll">
       <StatusBar />
 
-      <div className="p-24">
+      <div data-testid="canvases" className="p-24">
         <h1 className="text-4xl text-text-color font-semibold mb-8">Your Canvases</h1>
 
         <div className="grid grid-cols-4"> 
