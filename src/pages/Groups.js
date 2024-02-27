9c558e0400169cffaf4b6c1545362be00020d4d9
AdiKsOnDev <adilalizade13@gmail.com>
2024-02-28 00:00:18 +0400

    feat: Made basic group components
    Made Groups Card, and Groups List components

--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar.js';
+import GroupsList from '../components/Groups/GroupsList.js';
 
 function Groups() {
   return (
     <div className="w-screen h-full flex">
-      <Navbar page="home" />
+      <Navbar page="groups" />
+      <GroupsList />
     </div>
   )
 }
