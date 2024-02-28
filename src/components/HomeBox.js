d1cabc572c0a0d45506bc184270bc2225a6c536c
AdiKsOnDev <adilalizade13@gmail.com>
2024-02-28 12:34:10 +0400

    fix: Fixed the code to match Unit Tests

--- src/components/HomeBox.js
+++ src/components/HomeBox.js
@@ -9,61 +9,69 @@ import Project from "./Project.js";
 import Image from "../Assets/canvas.png";
  
 function HomeBox() {
-  const { currentUser } = useContext(AuthContext);
+  try {
+    const { currentUser } = useContext(AuthContext);
+  } catch (error) {
+    const { currentUser } = false;
+  }
   const [userFiles, setUserFiles] = useState([]);
   const [userCanvases, setUserCanvases] = useState([]);
 
-  useEffect(() => {
-    const getFiles = async () => {
-      const userRef = doc(collection(database, "Users"), currentUser.email);
-      const userSnapshot = await getDoc(userRef);
-      const user = userSnapshot.data();
-      console.log(user);
-      
-      setUserFiles(user.files);
-    };
+  try {
+    useEffect(() => {
+      const getFiles = async () => {
+        const userRef = doc(collection(database, "Users"), currentUser.email);
+        const userSnapshot = await getDoc(userRef);
+        const user = userSnapshot.data();
+        console.log(user);
+        
+        setUserFiles(user.files);
+      };
 
-    getFiles();
-  }, [currentUser]);
-  
-  useEffect(() => {
-    const getCanvases = async () => {
-      const userRef = doc(collection(database, "Users"), currentUser.email);
-      const userSnapshot = await getDoc(userRef);
-      const user = userSnapshot.data();
-      console.log(user);
-      
-      setUserCanvases(user.canvases);
-    };
+      getFiles();
+    }, [currentUser]);
+    
+    useEffect(() => {
+      const getCanvases = async () => {
+        const userRef = doc(collection(database, "Users"), currentUser.email);
+        const userSnapshot = await getDoc(userRef);
+        const user = userSnapshot.data();
+        console.log(user);
+        
+        setUserCanvases(user.canvases);
+      };
 
-    getCanvases();
-  }, [currentUser]);
+        getCanvases();
+      }, [currentUser]);
+    } catch(error) {
+      console.log(error);
+    }
 
-  return (
-    <div className="flex flex-col w-3/4 h-full bg-primary overflow-scroll">
-      <StatusBar />
+    return (
+      <div className="flex flex-col w-3/4 h-full bg-primary overflow-scroll">
+        <StatusBar />
 
-      <div data-testid="canvases" className="p-24">
-        <h1 className="text-4xl text-text-color font-semibold mb-8">Your Files</h1>
+        <div data-testid="files" className="p-24">
+          <h1 className="text-4xl text-text-color font-semibold mb-8">Your Files</h1>
 
-        <div className="grid grid-cols-4"> 
-          {userFiles.map((group) => (
-            <Project image={PreviewDocx} title={group} />
-          ))}
+          <div className="grid grid-cols-4"> 
+            {userFiles.map((group) => (
+              <Project image={PreviewDocx} title={group} />
+            ))}
+          </div>
+        </div>
+        
+        <div data-testid="canvases" className="p-24">
+          <h1 className="text-4xl text-text-color font-semibold mb-8">Your Canvases</h1>
+
+          <div className="grid grid-cols-4"> 
+            {userCanvases.map((canvas) => (
+              <Project image={PreviewImage} title={canvas} />
+            ))}
+          </div>
         </div>
       </div>
-      
-      <div data-testid="canvases" className="p-24">
-        <h1 className="text-4xl text-text-color font-semibold mb-8">Your Canvases</h1>
+    );
+  };
 
-        <div className="grid grid-cols-4"> 
-          {userCanvases.map((canvas) => (
-            <Project image={PreviewImage} title={canvas} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default HomeBox;
+  export default HomeBox;
