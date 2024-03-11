c59ffa508cc0d693b5e88817214a15157d77a0a7
AdiKsOnDev <adilalizade13@gmail.com>
2024-03-11 10:48:51 +0400

    fix: Fixed page width bein 3/4

--- src/components/TODO_List.js
+++ src/components/TODO_List.js
@@ -14,38 +14,38 @@ function TODOList() {
    const[theme,updateTheme]=useState('light')
 
     return (
-        <div className="flex flex-col w-3/4 h-full bg-primary overflow-scroll">
-          <StatusBar />
+      <div className="flex flex-col w-full h-full bg-primary overflow-scroll">
+        <StatusBar />
 
-            
-            <contextTodo.Provider value={{todos,dispatch,themes:[theme,updateTheme]}}>
+          
+          <contextTodo.Provider value={{todos,dispatch,themes:[theme,updateTheme]}}>
 
-          <div className="flex flex-col p-20 " >
-            <h1  className="text-4xl text-text-color font-semibold mb-8">TODO List</h1>
+        <div className="flex flex-col p-20" >
+          <h1  className="text-4xl text-text-color font-semibold mb-8">TODO List</h1>
+          <div className="flex flex-row">
             <div className="flex flex-row">
-              <div className="flex flex-row">
-                <DisplayStickyNotes/>
-              </div>
-          </div>
-          </div>
-          </contextTodo.Provider>
-
-    
-          <div className="p-24">
-            <h1 className="text-4xl text-text-color font-semibold mb-8">Saved Notes</h1>
-    
-            <div className="grid grid-cols-4 gap-4">  
-              <SavedNotes img={Img} heading="Monday" />
-              <SavedNotes img={Img} heading="Tuesday" />
-              <SavedNotes img={Img} heading="Wednesday" />
-              <SavedNotes img={Img} heading="Thursday" />
-              <SavedNotes img={Img} heading="Friday" />
-              <SavedNotes img={Img} heading="Saturday" />
-              <SavedNotes img={Img} heading="Sunday" />
-              <SavedNotes img={Img} heading="All Week" />
+              <DisplayStickyNotes/>
             </div>
+        </div>
+        </div>
+        </contextTodo.Provider>
+
+  
+        <div className="p-24">
+          <h1 className="text-4xl text-text-color font-semibold mb-8">Saved Notes</h1>
+  
+          <div className="grid grid-cols-4 gap-4">  
+            <SavedNotes img={Img} heading="Monday" />
+            <SavedNotes img={Img} heading="Tuesday" />
+            <SavedNotes img={Img} heading="Wednesday" />
+            <SavedNotes img={Img} heading="Thursday" />
+            <SavedNotes img={Img} heading="Friday" />
+            <SavedNotes img={Img} heading="Saturday" />
+            <SavedNotes img={Img} heading="Sunday" />
+            <SavedNotes img={Img} heading="All Week" />
           </div>
         </div>
+      </div>
       );
     };
 export default TODOList;
