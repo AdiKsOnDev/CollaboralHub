2ed759006af37bdef87c33fe3a84796ca8b3f29b
AdiKsOnDev <adilalizade13@gmail.com>
2024-03-11 11:22:23 +0400

    feat: Changed the styling of notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,7 +9,7 @@ function Note(props) {
   return (
     <div className="w-[198px] h-[252px] bg-white rounded-[15px] shadow flex flex-col justify-between p-2 ">
       <h1 className="p-2 text-xl text-accent-red underline font-semibold">{props.title}</h1>
-      <p>{props.content}</p>
+      <p className="p-2">{props.content}</p>
       <button className="bg-accent-red hover:bg-accent-blue duration-300 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center" onClick={handleClick}> Done </button>
     </div>
   );
