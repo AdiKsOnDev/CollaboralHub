ada550e162f22a1fd2768b55d7381c049bc4dbe5
AdiKsOnDev <adilalizade13@gmail.com>
2024-03-12 17:35:32 +0400

    fix: Made the doc name larger

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ function Project({image, title, id, owner, date}) {
     <a href={id} className="flex flex-col items-center justify-center mr-5 mb-12 w-52 hover:w-60 hover:text-lg duration-300">
       <img className="w-full h-30 rounded-lg" src={image} alt="Canvas" />
       
-      <h1 className="text-text-color text-md font-semibold w-full mb-2 pl-2 mt-3">{title}</h1>
+      <h1 className="text-text-color text-lg font-semibold w-full mb-2 pl-2 mt-3">{title}</h1>
       <h2 className="text-text-color text-sm font-light italic pl-2 w-full">Accessed by: {owner}</h2>
       <h3 className="text-text-color text-sm font-light italic pl-2 w-full">Last Edited: {date}</h3>
     </a>
