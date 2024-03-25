828441291ba7a7b1ac6320b92c38071d3a468db8
AdiKsOnDev <adilalizade13@gmail.com>
2024-03-25 13:06:19 +0400

    fix: FIxed user pfp not showing

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ReactComponent as ProfileSVG } from "../Assets/Profile_Button.svg";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { database } from "../firebase.js";
 import { AuthContext } from '../context/AuthContext';
 import { auth } from "../firebase.js";
 import LogoutButton from "./LogoutButton.js";
@@ -9,15 +11,25 @@ import { useNavigate } from 'react-router-dom';
 
 function ProfilePicture() {
   const navigate= useNavigate();
+  const { currentUser } = useContext(AuthContext);
+  const [userData, setUserData] = useState(""); 
   const routeChange = () =>{ 
     navigate("/DisplayProfile");
   };
 
-  try {
-    const currentUser = useContext(AuthContext);
-  } catch(err) {
-    console.log("No User");
-  } 
+  useEffect(() => {
+    const getUser = async () => {
+      const userRef = doc(collection(database, "Users"), currentUser.email);
+      const userSnapshot = await getDoc(userRef);
+      const user = userSnapshot.data();
+
+      setUserData(user);
+      console.log(userData.profileImg);
+    }
+
+    getUser();
+  }, [currentUser])
+
   const [menu, setMenu] = useState(false);
 
   const handleToggle = () => {
@@ -26,7 +38,7 @@ function ProfilePicture() {
 
   return (
     <div>
-      <img src="" alt="profile img" className="rounded-full w-12 h-12 border-accent-red border-4" onClick={handleToggle} />
+      <img src={userData.profileImg} alt="profile img" className="rounded-full w-12 h-12 border-accent-red border-4" onClick={handleToggle} />
 
       <ul className={`${!menu ? "hidden" : "absolute top-24 right-0" }`}> 
          <li className="w-full py-5 text-white flex bg-secondary justify-center font-semibold rounded-t-xl hover:bg-placeholder duration-300" onClick={routeChange} ><ProfileSVG className="w-6 h-6 mr-3"/>Profile</li>
